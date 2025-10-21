349c49566a7540cb8b954c5f12287a4bbe0a8320
Egor Kunovsky <egorka@gmail.com>
2025-10-21 16:18:16 +0100

    PermissionsKit resolution

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
       .package(url: "https://github.com/dDomovoj/StackCraft.git", .upToNextMajor(from: "0.3.3")),
 
       .package(url: "https://github.com/eddiekaiger/SwiftyAttributes", branch: "master"),
-      .package(url: "https://github.com/sparrowcode/PermissionsKit", .upToNextMajor(from: "11.1.0")),
+      .package(url: "https://github.com/sparrowcode/PermissionsKit", .upToNextMajor(from: "11.1")),
       .package(url: "https://github.com/layoutBox/PinLayout", .upToNextMajor(from: "1.10.3")),
       .package(url: "https://github.com/RevenueCat/purchases-ios.git", .upToNextMajor(from: "5.25.2")),
       .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.2.4")),
