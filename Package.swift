866f13fd8bad387cd87d13ecdf5eeb1809089589
Egor Kunovsky <egorka@gmail.com>
2025-10-21 16:04:47 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
       .package(url: "https://github.com/layoutBox/PinLayout", .upToNextMajor(from: "1.10.3")),
       .package(url: "https://github.com/RevenueCat/purchases-ios.git", .upToNextMajor(from: "5.25.2")),
       .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.2.4")),
-      .package(url: "https://github.com/sereivoanyong/SVProgressHUD.git", branch: "master"),
+      .package(url: "https://github.com/sereivoanyong/SVProgressHUD.git", branch: "main),
       .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", .upToNextMajor(from: "6.16.2")),
     ],
     targets: [
